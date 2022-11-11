b85ca962ae1ba5e9933fe5ea2664b7440e835573
quan <lehoangquan@gmail.com>
2022-11-11 09:28:31 +0700

    loadingpage

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,17 +1,34 @@
 import React from "react";
 import gif from "../assets/images/LOADINGFULL6s.gif";
 import { motion } from "framer-motion";
-
+import { makeStyles } from "@mui/styles";
+const useStyles = makeStyles((theme) => ({
+  gif: {
+    position: "relative",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    maxWidth: "80%",
+  },
+  "@media (max-width: 640px)": {
+    image: {
+      marginTop: "300px",
+      maxWidth: "100%",
+    },
+  },
+}));
 const LoadingPage = () => {
+  const classes = useStyles();
   return (
     <motion.div
       initial={{ opacity: 1 }}
       animate={{ opacity: 0 }}
       transition={{ delay: 5, duration: 1 }}
-      style={{ position: "relative", display: "flex", justifyContent: "center", alignItems: "center" }}>
-
-      <img src={gif} alt="" style={{ maxWidth: "70%" }} />
-
+      className={classes.gif}
+    >
+      <img src={gif} alt="" className={classes.image} />
     </motion.div>
   );
 };
